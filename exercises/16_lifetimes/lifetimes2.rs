b7441fb578f04840334132279ef0dd14dd961374
Jepbura <jepbura@gmail.com>
2024-08-25 16:46:51 +0330

    New update

--- exercises/16_lifetimes/lifetimes2.rs
+++ exercises/16_lifetimes/lifetimes2.rs
@@ -10,10 +10,18 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 fn main() {
     // TODO: Fix the compiler error by moving one line.
 
-    let string1 = String::from("long string is long");
+    let string1 = String::from("long string is long1");
     let result;
     {
         let string2 = String::from("xyz");
+        result = longest(&string1, &string2);
+        println!("The longest string is '{result}'");
+    }
+
+    let string1 = String::from("long string is long2");
+    let string2 = String::from("xyz");
+    let result;
+    {
         result = longest(&string1, &string2);
     }
     println!("The longest string is '{result}'");
