b714b72e3a0926ff2db3aa2c5fc4e9edd1956122
jepbura <jepbura@gmail.com>
2024-08-31 21:42:42 +0330

    New update

--- exercises/19_smart_pointers/cow1.rs
+++ exercises/19_smart_pointers/cow1.rs
@@ -26,8 +26,8 @@ mod tests {
     #[test]
     fn reference_mutation() {
         // Clone occurs because `input` needs to be mutated.
-        let vec = vec![-1, 0, 1];
-        let mut input = Cow::from(&vec);
+        let slice = [-1, 0, 1];
+        let mut input = Cow::from(&slice[..]);
         abs_all(&mut input);
         assert!(matches!(input, Cow::Owned(_)));
     }
@@ -35,35 +35,34 @@ mod tests {
     #[test]
     fn reference_no_mutation() {
         // No clone occurs because `input` doesn't need to be mutated.
-        let vec = vec![0, 1, 2];
-        let mut input = Cow::from(&vec);
+        let slice = [0, 1, 2];
+        let mut input = Cow::from(&slice[..]);
+
         abs_all(&mut input);
-        // TODO: Replace `todo!()` with `Cow::Owned(_)` or `Cow::Borrowed(_)`.
-        assert!(matches!(input, todo!()));
+        assert!(matches!(input, Cow::Borrowed(_)));
     }
 
     #[test]
     fn owned_no_mutation() {
-        // We can also pass `vec` without `&` so `Cow` owns it directly. In this
-        // case, no mutation occurs (all numbers are already absolute) and thus
-        // also no clone. But the result is still owned because it was never
-        // borrowed or mutated.
-        let vec = vec![0, 1, 2];
-        let mut input = Cow::from(vec);
+        // We can also pass `slice` without `&` so Cow owns it directly. In this
+        // case no mutation occurs and thus also no clone, but the result is
+        // still owned because it was never borrowed or mutated.
+        let slice = vec![0, 1, 2];
+        let mut input = Cow::from(slice);
+
         abs_all(&mut input);
-        // TODO: Replace `todo!()` with `Cow::Owned(_)` or `Cow::Borrowed(_)`.
-        assert!(matches!(input, todo!()));
+        assert!(matches!(input, Cow::Owned(_)));
     }
 
     #[test]
     fn owned_mutation() {
-        // Of course this is also the case if a mutation does occur (not all
-        // numbers are absolute). In this case, the call to `to_mut()` in the
-        // `abs_all` function returns a reference to the same data as before.
-        let vec = vec![-1, 0, 1];
-        let mut input = Cow::from(vec);
+        // Of course this is also the case if a mutation does occur. In this
+        // case the call to `to_mut()` in the abs_all() function returns a
+        // reference to the same data as before.
+        let slice = vec![-1, 0, 1];
+        let mut input = Cow::from(slice);
+
         abs_all(&mut input);
-        // TODO: Replace `todo!()` with `Cow::Owned(_)` or `Cow::Borrowed(_)`.
-        assert!(matches!(input, todo!()));
+        assert!(matches!(input, Cow::Owned(_)));
     }
 }
